aa9453f76aca4fd2b2de73a0c5526371649f4174
Qathom <tony9295@gmail.com>
2018-05-18 13:02:00 +0200

    Use default executable path of puppeteer

--- src/renderer/browser/index.js
+++ src/renderer/browser/index.js
@@ -1,22 +1,10 @@
 import puppeteer from 'puppeteer';
-import log from 'electron-log';
 
-export default async function () {
-  const browserFetcher = puppeteer.createBrowserFetcher();
-  const localChromiums = await browserFetcher.localRevisions();
+export default {
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true,
+  timeout: 60000,
+  slowMo: 100,
+  executablePath: puppeteer.executablePath(),
+};
 
-  if (localChromiums.length === 0) {
-    log.warn('no local chromium found.');
-    return null;
-  }
-
-  const { executablePath } = await browserFetcher.revisionInfo(localChromiums[0]);
-
-  return {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true,
-    timeout: 60000,
-    slowMo: 100,
-    executablePath,
-  };
-}
